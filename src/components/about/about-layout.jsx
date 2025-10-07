3937a3b96166cb92a4e53eea4a98d395f7da66e4
Max Semionov <semenov.max.art@mail.ru>
2025-10-07 10:47:50 +0300

    Worked on gh-pages deploy

--- src/components/about/about-layout.jsx
+++ src/components/about/about-layout.jsx
@@ -48,35 +48,19 @@ export function AboutLayout({
                     {messages}
                 </div>
             </MediaQuery>
-            {/* <MediaQuery minWidth={1200}>
-                <img
-                    src={totemImageSrc}
-                    alt={totemImageAlt}
-                    className="absolute -top-24 right-16 w-1/4"
-                />
-                <div className="ml-[10%] text-3xl uppercase font-medium w-3/5 wrap-normal text-center">
-                    {text1} {text2}
+            <MediaQuery maxWidth={1199} minWidth={550}>
+                <div className="text-5xl w-4/5 mx-auto text-center uppercase font-bold">
+                    этот сайт пока не поддерживает это разрешение экрана
                 </div>
-                <div className="mt-13 ml-[20%] text-3xl uppercase font-medium w-1/2 wrap-normal text-center">
-                    {text3} {text4}
+                <div className="mt-10 w-4/5 text-5xl text-center mx-auto uppercase font-bold">
+                    <a
+                        href="https://www.youtube.com/watch?v=iPBq1pTGgro"
+                        className="px-10 py-6 bg-neutral-300 hover:bg-neutral-400 underline rounded-full"
+                    >
+                        простите(
+                    </a>
                 </div>
-                <AboutMeWindow
-                    className="ml-[5%] -mt-40 w-[12%]!"
-                    text={aboutText1}
-                >
-                    <img src={aboutSrc1} alt={aboutAlt1} />
-                </AboutMeWindow>
-                <AboutMeWindow
-                    className=" ml-[75%] -mt-10 w-[15%]!"
-                    text={aboutText2}
-                >
-                    <img src={aboutSrc2} alt={aboutAlt2} />
-                </AboutMeWindow>
-
-                <div className="w-[890px] mx-auto flex flex-col gap-10 mt-5">
-                    {messages}
-                </div>
-            </MediaQuery> */}
+            </MediaQuery>
             <MediaQuery maxWidth={549}>
                 <div className="grid grid-cols-2 gap-x-5 px-7 gap-y-11 text-3xl font-medium wrap-normal text-center">
                     <div className="flex flex-col gap-11 pt-10">
@@ -101,10 +85,7 @@ export function AboutLayout({
                     <div className="col-span-2 uppercase">{text3}</div>
                     <div className="uppercase">{text4}</div>
                     <div className="">
-                        <AboutMeWindow
-                            className="mt-20"
-                            text={aboutText2}
-                        >
+                        <AboutMeWindow className="mt-20" text={aboutText2}>
                             <img src={aboutSrc2} alt={aboutAlt2} />
                         </AboutMeWindow>
                     </div>
--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -121,52 +121,59 @@ const miniWorks = [
 export function Works() {
     return (
         <>
-        <MediaQuery minWidth={1200}>
-            <WorksLayout
-                worksTitle={"Последние проекты"}
-                worksContent={works.map((item, index) => (
-                    <WorkElement
-                        info={item}
-                        key={index}
-                        className={index !== 0 && "mt-11"}
-                    />
-                ))}
-                miniWorksTitle={"Мини-проекты"}
-                miniWorksContent={
-                    <Carousel>
-                        {miniWorks.map((item, index) => (
-                            <MiniWorkElement info={item} key={index} />
-                        ))}
-                    </Carousel>
-                }
-            />
-        </MediaQuery>
-        <MediaQuery maxWidth={1199} minWidth={550}>
-
-        </MediaQuery>
-        <MediaQuery maxWidth={549}>
-            <WorksLayout
-                worksTitle={"Последние проекты"}
-                worksContent={works.map((item, index) => (
-                    <SmWorkElement
-                        info={item}
-                        key={index}
-                        className={index !== 0 && "mt-11"}
-                    />
-                ))}
-                miniWorksTitle={"Мини-проекты"}
-                miniWorksContent={
-                    <Carousel>
-                        {miniWorks.map((item, index) => (
-                            <SmMiniWorkElement info={item} key={index} />
-                        ))}
-                    </Carousel>
-                }
-            />
-        </MediaQuery>
-               </>
-
+            <MediaQuery minWidth={1200}>
+                <WorksLayout
+                    worksTitle={"Последние проекты"}
+                    worksContent={works.map((item, index) => (
+                        <WorkElement
+                            info={item}
+                            key={index}
+                            className={index !== 0 && "mt-11"}
+                        />
+                    ))}
+                    miniWorksTitle={"Мини-проекты"}
+                    miniWorksContent={
+                        <Carousel>
+                            {miniWorks.map((item, index) => (
+                                <MiniWorkElement info={item} key={index} />
+                            ))}
+                        </Carousel>
+                    }
+                />
+            </MediaQuery>
+            <MediaQuery maxWidth={1199} minWidth={550}>
+                <div className="text-5xl w-4/5 mx-auto text-center uppercase font-bold">
+                    этот сайт пока не поддерживает это разрешение экрана
+                </div>
+                <div className="mt-10 w-4/5 text-5xl text-center mx-auto uppercase font-bold">
+                    <a
+                        href="https://www.youtube.com/watch?v=iPBq1pTGgro"
+                        className="px-10 py-6 bg-neutral-300 hover:bg-neutral-400 underline rounded-full"
+                    >
+                        простите(
+                    </a>
+                </div>
+            </MediaQuery>
+            <MediaQuery maxWidth={549}>
+                <WorksLayout
+                    worksTitle={"Последние проекты"}
+                    worksContent={works.map((item, index) => (
+                        <SmWorkElement
+                            info={item}
+                            key={index}
+                            className={index !== 0 && "mt-11"}
+                        />
+                    ))}
+                    miniWorksTitle={"Мини-проекты"}
+                    miniWorksContent={
+                        <Carousel>
+                            {miniWorks.map((item, index) => (
+                                <SmMiniWorkElement info={item} key={index} />
+                            ))}
+                        </Carousel>
+                    }
+                />
+            </MediaQuery>
+        </>
     );
 }
-
-
